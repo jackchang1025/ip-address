12904bb6d2248d8d764d0164488a91d4e8eb79c8
weijiajia <123456@qq.com>
2025-04-18 22:45:25 +0800

    在Request类的handleRetry方法中新增对ForbiddenException的处理，以进一步增强请求的容错能力和稳定性。

--- src/Request.php
+++ src/Request.php
@@ -16,6 +16,7 @@ use Saloon\Contracts\DataObjects\WithResponse;
 use Saloon\Exceptions\Request\FatalRequestException;
 use Saloon\Exceptions\Request\RequestException;
 use Saloon\Exceptions\Request\ServerException;
+use Saloon\Exceptions\Request\Statuses\ForbiddenException;
 abstract class Request extends SoloRequest implements HasLoggerInterface,ProxyManagerInterface,RequestContract
 {
     use HasLogger;
@@ -27,7 +28,7 @@ abstract class Request extends SoloRequest implements HasLoggerInterface,ProxyMa
 
     public function handleRetry(FatalRequestException|RequestException $exception, SaloonRequest $request): bool
     {
-        return $exception instanceof FatalRequestException || $exception instanceof ServerException;
+        return $exception instanceof FatalRequestException || $exception instanceof ServerException || $exception instanceof ForbiddenException;
     }
 
 
