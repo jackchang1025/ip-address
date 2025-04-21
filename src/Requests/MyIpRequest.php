6e9254122eede81a046b9f919425557f67cecfb2
weijiajia <123456@qq.com>
2025-04-21 23:23:05 +0800

    在MyIpRequest类中更新默认请求头，优化了Accept、Accept-Encoding和Accept-Language字段，并新增Upgrade-Insecure-Requests头，以提高请求的兼容性和性能。

--- src/Requests/MyIpRequest.php
+++ src/Requests/MyIpRequest.php
@@ -12,7 +12,6 @@ class MyIpRequest extends Request
 {
     protected Method $method = Method::GET;
 
-
     public function resolveEndpoint(): string
     {
         return 'http://api.ip.cc';
@@ -21,9 +20,9 @@ class MyIpRequest extends Request
     public function defaultHeaders(): array
     {
         return [
-           'Accept'             => '*/*',
-            'Accept-Encoding'    => 'gzip, deflate, br, zstd',
-            'Accept-Language'    => 'en,zh-CN;q=0.9,zh;q=0.8',
+           'Accept'             => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'Accept-Encoding'    => 'gzip, deflate',
+            'Accept-Language'    => 'en-US,en;q=0.9',
             'Connection'         => 'keep-alive',
             'Sec-Ch-Ua'          => '"Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"',
             'Sec-Ch-Ua-Mobile'   => '?0',
@@ -31,6 +30,7 @@ class MyIpRequest extends Request
             'Sec-Fetch-Dest'     => 'empty',
             'Sec-Fetch-Mode'     => 'cors',
             'Sec-Fetch-Site'     => 'same-site',
+            'Upgrade-Insecure-Requests' => '1',
             'User-Agent'         => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36',
         ];
     }
